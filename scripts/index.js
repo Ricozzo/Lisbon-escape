b1973812c194345e243daccfa9c7dab454500b76
Ricozzo <henriqueortizpereira@gmail.com>
2023-04-21 17:24:30 +0100

    test-git

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+// JS Initialization
+console.log("JS is loaded");
+
+// Canvas Initialization
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// Start Button
+const startButton = document.getElementById("start");
+
+// Create tha Player
+const player = new Component(0, 525, 75, 75, "red", ctx);
+
+//Start Button on Click
+startButton.onclick = function () {
+  console.log("start");
+  const game = new Game(ctx, canvas.width, canvas.height, player);
+  game.start();
+};
+
+// Move the Player
+document.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "ArrowLeft":
+      player.speedX -= 1;
+      break;
+    case "ArrowRight":
+      player.speedX += 1;
+      break;
+  }
+});
+
+// Stop Speed
+document.addEventListener("keyup", () => {
+  player.speedX = 0;
+  player.speedY = 0;
+});
